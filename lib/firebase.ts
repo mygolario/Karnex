a900d612bf1d5969306245bf422b1f6c5e78d696
mygolario <ariokaveh85@gmail.com>
2025-12-19 19:23:58 +0330

    fix: remove hardcoded firebase secret

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,15 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCmCyXBlANcuBZhRIF9QZemzC4-UcFgVoY",
-  authDomain: "karnex-aaec4.firebaseapp.com",
-  projectId: "karnex-aaec4",
-  storageBucket: "karnex-aaec4.firebasestorage.app",
-  messagingSenderId: "882483651592",
-  appId: "1:882483651592:web:f08f1ed916453cb2a19dbd",
-  measurementId: "G-KMVWDV0PLM"
-};
+const firebaseConfigStr = process.env.NEXT_PUBLIC_FIREBASE_CONFIG;
+
+if (!firebaseConfigStr) {
+  throw new Error('NEXT_PUBLIC_FIREBASE_CONFIG is not defined');
+}
+
+const firebaseConfig = JSON.parse(firebaseConfigStr);
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
